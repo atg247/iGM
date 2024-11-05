654d02229a9f435530e2b86df4b5690da8d36e2c
Antti <malkamaki.antti@gmail.com>
2024-11-05 22:50:17 +0200

    SQL-perusteet Herokulle.

--- config.py
+++ config.py
@@ -3,8 +3,11 @@ from datetime import timedelta
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')  # Get from environment for security
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///hockey_data.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    #SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///hockey_data.db')
+    #SQLALCHEMY_TRACK_MODIFICATIONS = False
     
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///local_database.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     REMEMBER_COOKIE_DURATION = timedelta(days=3)  # User session will be remembered for 7 days
     REMEMBER_COOKIE_SECURE = True  # Set to True if you have HTTPS enabled
