97c0e04fa6ba633012a9ef2e83c196e4047638f5
Antti <malkamaki.antti@gmail.com>
2024-11-05 23:59:15 +0200

    -

--- config.py
+++ config.py
@@ -7,7 +7,14 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')  # Fallback for development
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///local_database.db')
+
+    # Get the DATABASE_URL and modify if necessary
+    uri = os.getenv('DATABASE_URL', 'sqlite:///local_database.db')
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    
+    SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     REMEMBER_COOKIE_DURATION = timedelta(days=3)
-    REMEMBER_COOKIE_SECURE = True  # TRUE Herokussa, False paikallisesti
+    REMEMBER_COOKIE_SECURE = True  # True for Heroku, False locally
